14ac14a6ff78a2de6c3be2146e3a7a72eb521709
Mital Joshi <tbs.mital@gmail.com>
2019-03-25 18:36:47 +0530

    Added dog breed conversion.

--- res/index.js
+++ res/index.js
@@ -1,48 +1,71 @@
 var data, mainCanvas, hdpiCanvas, mdpiCanvas, xhdpiCanvas, xxhdpiCanvas, xxxhdpiCanvas;
 function handleFileSelect(evt) {
-    var file = evt.target.files[0];
-    if (file != undefined) {
-        var ext = file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2);
-        console.log(ext);
-        if (ext == 'csv') {
-            Papa.parse(file, {
-                header: true,
-                dynamicTyping: true,
-                complete: function (results) {
-                    createJSON(results, function (isSuccess, outputObj) {
-                        if (isSuccess) {
-                            $("#status").html("Pass<br />").css("color", "green");
-                            // console.log(quotes);
-                            $("#status").append(JSON.stringify(outputObj, null, 4));
-                            console.log(JSON.stringify(outputObj, null, 4));
-                            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(outputObj));
-                            setUrl($("#downloadJsonLink"), dataStr, "data.json");
-                            console.log("canvas generated.");
-                            $("#updateBtnDiv").show();
-                        }
-                        else {
-                            $("#status").html("Please choose file containing id,quote,author and wallpaper data.").css("color", "red");
-                        }
-                    })
-
-                }
-            });
-        }
-        else {
+    var selectedApp = $("#app-selector").val();
+    if (selectedApp != '') {
+        var file = evt.target.files[0];
+        if (file != undefined) {
+            var ext = file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2);
+            console.log(ext);
+            if (ext == 'csv') {
+                Papa.parse(file, {
+                    header: true,
+                    dynamicTyping: true,
+                    complete: function (results) {
+                        createJSON(selectedApp, results, function (isSuccess, outputObj) {
+                            if (isSuccess) {
+                                $("#status").html("SUCCESS<br />").css("color", "green");
+                                // console.log(quotes);
+                                $("#status").append(JSON.stringify(outputObj, null, 4));
+                                console.log(JSON.stringify(outputObj, null, 4));
+                                var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(outputObj));
+                                setUrl($("#downloadJsonLink"), dataStr, "data.json");
+                                console.log("canvas generated.");
+                                $("#updateBtnDiv").show();
+                            }
+                            else {
+                                if (selectedApp == 'quotesApp') {
+                                    $("#status").html("For QUOTES-APP Application, Please choose file containing id,quote,author and wallpaper data.").css("color", "red");
+                                } else {
+                                    $("#status").html("For DOG-BREED Application, Please choose file containing id,name,image,description and isFav data.").css("color", "red");
+                                }
+                            }
+                        })
+                    }
+                });
+            }
+            else {
+                $("#status").html("Please choose CSV file.").css("color", "red");
+            }
+        } else {
             $("#status").html("Please choose CSV file.").css("color", "red");
         }
+    } else {
+        $("#status").html("Please choose application.").css("color", "red");
     }
 }
 
-function createJSON(results, callback) {
+function createJSON(selectedApp, results, callback) {
     data = results;
     console.log(data);
     console.log(data.meta.fields);
-    var arr = data.meta.fields;
+
+    if (selectedApp == 'dogBreed') {
+        getBreedsObj(data, function (isDone, outputObj) {
+            callback(isDone, outputObj);
+        })
+    } else {
+        getQuotesObj(data, function (isDone, outputObj) {
+            callback(isDone, outputObj);
+        });
+    }
+
+}
+function getQuotesObj(data, callback) {
     var outputObj = {};
     var quoteArray = [];
     var authorArray = [];
     var myData = data.data;
+    var arr = data.meta.fields;
     if (arr.length == 9 && arr.indexOf("id") >= 0 && arr.indexOf("category") >= 0 && arr.indexOf("author") >= 0 && arr.indexOf("quote") >= 0 && arr.indexOf("wallpaper") >= 0 && arr.indexOf("authorid") >= 0 && arr.indexOf("authorbio") >= 0 && arr.indexOf("authorimage") >= 0 && arr.indexOf("authorshortbio") >= 0) {
         for (var i = 1; i <= myData.length; i++) {
             var j = i - 1;
@@ -69,18 +92,43 @@ function createJSON(results, callback) {
                         if (authorArray[k].author_id == myData[j].authorid) {
                             isExists = true;
                         } else {
-                            if (k == (authorArray.length - 1) && isExists == false){
+                            if (k == (authorArray.length - 1) && isExists == false) {
                                 authorArray.push(authors);
                             }
                         }
                     }
-                }else{
+                } else {
                     authorArray.push(authors);
                 }
             }
         }
         outputObj.quotes = quoteArray;
         outputObj.authors = authorArray;
+        callback(true, outputObj);
+    } else {
+        callback(false, {});
+    }
+}
+function getBreedsObj(data, callback) {
+    var outputObj = {};
+    var breedArray = [];
+    var myData = data.data;
+    var arr = data.meta.fields;
+    if (arr.length == 5 && arr.indexOf("id") >= 0 && arr.indexOf("name") >= 0 && arr.indexOf("description") >= 0 && arr.indexOf("image") >= 0 && arr.indexOf("isFav") >= 0) {
+        for (var i = 1; i <= myData.length; i++) {
+            var j = i - 1;
+            var breedObj = {
+                "id": myData[j].id,
+                "name": myData[j].name,
+                "image": myData[j].image,
+                "description": myData[j].description,
+                "isFav": myData[j].isFav
+            }
+            if (breedObj.id != "") {
+                breedArray.push(breedObj);
+            }
+        }
+        outputObj.breedData = breedArray;
         callback(true, outputObj);
     } else {
         callback(false, {});
